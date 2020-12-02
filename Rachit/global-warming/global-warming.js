839672f8300f3bfae131185bcb096b9715b294f2
rexj8 <rachit.gla_cs18@gla.ac.in>
2020-12-02 15:35:53 +0530

    coding

---
+++ Rachit/global-warming/global-warming.js
@@ -0,0 +1,34 @@
+// ---------------------------------Sticky Navbar with Proress Bar------------------------------------------------------------------------------
+
+
+let progress = document.getElementById('progressbar');
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+
+    let progressHeight = (window.pageYOffset / totalHeight) * 100;
+    progress.style.height = progressHeight + "%";
+
+    var x = document.getElementById("sticky_navbar");
+
+    if (document.body.scrollTop > 54 || document.documentElement.scrollTop > 54) {
+        // document.getElementById("sticky_navbar").className = "sticky-navbar-properties";
+
+            x.style.display = "flex";
+            x.style.position = "fixed";
+            x.style.background = "linear-gradient(to right,rgb(225, 226, 210),rgb(224, 223, 203),rgb(219, 216, 199),rgb(214, 211, 192))";
+            x.style.alignContent = "center";
+            x.style.justifyContent = "space-around";
+            x.style.width = "100%";
+            x.style.minHeight = "8vh";
+            x.style.top = "0";
+            x.style.left = "-80";
+        
+    } else {
+        // document.getElementById("sticky_navbar").className = "";
+        x.style.display = "none";
+    }
+}
+// ----------------------------------------------------------------------------------------------------------------------------
