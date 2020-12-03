07b5c6afbb170d7b057c23d0b9bbc3c8f9035c33
rexj8 <rachit.gla_cs18@gla.ac.in>
2020-12-03 12:33:01 +0530

    working on faded the banner

--- Rachit/global-warming/global-warming.js
+++ Rachit/global-warming/global-warming.js
@@ -63,29 +63,8 @@ navSlide();
 // ----------------------------------------------------------------------------------------------------------------------------
 
 // ---------------------------------Burger sign------------------------------------------------------------------------------
-const navSlide = () => {
-    
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = ''
-            }
-            else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-            }
-        });
-
-        burger.classList.toggle('toggle');
-        
-    });
-
-}
-
-navSlide();
+const bg = document.getElementById('bear');
+        window.addEventListener('scroll',function(){
+            bg.style.opacity = 1 - + window.pageYOffset/1100 + '';
+        })
 // ----------------------------------------------------------------------------------------------------------------------------
