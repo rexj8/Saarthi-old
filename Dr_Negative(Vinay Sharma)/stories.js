ded91742c2ea4f35c328be2244c111fa6179faab
Vinay Sharma <45567648+DR-NEGATIVE@users.noreply.github.com>
2020-12-04 12:05:15 +0530

    Update stories.js

--- Dr_Negative(Vinay Sharma)/stories.js
+++ Dr_Negative(Vinay Sharma)/stories.js
@@ -37,10 +37,12 @@ const navSlide = () => {
 
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
+        navLinks.style='color:black';
 
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
                 link.style.animation = ''
+
             }
             else {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
