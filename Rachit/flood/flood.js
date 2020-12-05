6977b0a37761c9f31edc3e98b7aa2e37982973dc
rexj8 <rachit.gla_cs18@gla.ac.in>
2020-12-05 07:40:42 +0530

    working on banner

--- Rachit/flood/flood.js
+++ Rachit/flood/flood.js
@@ -65,12 +65,12 @@ navSlide();
 // ----------------------------------------------------------------------------------------------------------------------------
 
 // ---------------------------------Parallex scrolling------------------------------------------------------------------------------
-<script>
-        const bg = document.getElementById('header');
-        window.addEventListener('scroll', function () {
-            bg.style.backgroundSize = 150 - + window.pageYOffset / 12 + '%';
-            bg.style.opacity = 1 - + window.pageYOffset / 700 + '';
 
-        })
-    </script>
+const bg = document.getElementById('header');
+window.addEventListener('scroll', function () {
+    bg.style.backgroundSize = 150 - + window.pageYOffset / 12 + '%';
+    bg.style.opacity = 1 - + window.pageYOffset / 700 + '';
+
+})
+   
 // ----------------------------------------------------------------------------------------------------------------------------
